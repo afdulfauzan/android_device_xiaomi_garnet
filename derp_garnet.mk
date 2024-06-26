2da6fcbdb94121ad11a4ce9ac27a5ca792530dbc
moewob <afdlfzn@gmail.com>
2024-06-27 02:51:06 +0700

    garnet: drop Evolution X

--- derp_garnet.mk
+++ derp_garnet.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
+# Inherit some common DerpFest stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
